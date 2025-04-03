e69f2733641daf7ce25129799313bb6206a61511
makeit <115435212@qq.com>
2025-04-03 16:51:51 +0800

    Simplify sync mode check for non-Optimism chains
    Removed the condition checking BedrockBlock for Optimism chains in the sync mode logic. This change ensures a cleaner state check logic and allows uniform handling across all chain configurations.

--- eth/handler.go
+++ eth/handler.go
@@ -169,7 +169,8 @@ func newHandler(config *handlerConfig) (*handler, error) {
 		}
 	} else {
 		head := h.chain.CurrentBlock()
-		if head.Number.Uint64() > 0 && h.chain.HasState(head.Root) && (!config.Chain.Config().IsOptimism() || head.Number.Cmp(config.Chain.Config().BedrockBlock) != 0) {
+		//if head.Number.Uint64() > 0 && h.chain.HasState(head.Root) && (!config.Chain.Config().IsOptimism() || head.Number.Cmp(config.Chain.Config().BedrockBlock) != 0) {
+		if head.Number.Uint64() > 0 && h.chain.HasState(head.Root) {
 			// Print warning log if database is not empty to run snap sync.
 			// For OP chains, snap sync from bedrock block is allowed.
 			log.Warn("Switch sync mode from snap sync to full sync")
